f32043cc025288f7bf17c5640b09ef58ee037e33
yang <juliansalvador.dev@gmail.com>
2026-07-12 00:34:25 +0800

    feat(content): add static content (main links)

---
+++ src/content/shared.content.ts
@@ -0,0 +1,24 @@
+import type { NavLink } from "../types/shared.types";
+
+export const mainLinks: NavLink[] = [
+  {
+    label: "New Arrivals",
+    href: "/products/new-arrivals",
+  },
+  {
+    label: "Men",
+    href: "/products/men",
+  },
+  {
+    label: "Women",
+    href: "/products/women",
+  },
+  {
+    label: "Kids",
+    href: "/products/kids",
+  },
+  {
+    label: "Sale",
+    href: "/products/sale",
+  },
+];
