cd019929ac9ddaa3c8f48b5c6ed0e92426a2584b
yang <juliansalvador.dev@gmail.com>
2026-07-13 23:08:21 +0800

    feat(routes): add products route to app router

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,15 @@
 import { createBrowserRouter } from "react-router";
 import AppLayout from "./layouts/AppLayout";
 import Home from "./pages/Home";
+import CatalogLayout from "./layouts/CatalogLayout";
 
 export const app_routes = createBrowserRouter([
   {
     path: "/",
     Component: AppLayout,
-    children: [{ index: true, Component: Home }],
+    children: [
+      { index: true, Component: Home },
+      { path: "/products", Component: CatalogLayout },
+    ],
   },
 ]);
