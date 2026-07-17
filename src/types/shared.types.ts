b9052f462f143f25d19fce91569a8b2389626816
yang <juliansalvador.dev@gmail.com>
2026-07-18 03:34:19 +0800

    feat(types): update types

--- src/types/shared.types.ts
+++ src/types/shared.types.ts
@@ -3,3 +3,22 @@ export interface NavLink {
   href: string;
   children?: NavLink[];
 }
+export interface CategorizedNavLink {
+  category: string;
+  links: NavLink[];
+}
+
+export interface ImageProps {
+  src: string;
+  alt: string;
+  width?: number;
+  height?: number;
+}
+
+export interface CalloutCardProps {
+  headline: string;
+  description?: string;
+  imageUrl?: string;
+  to?: string;
+  ctaTitle?: string;
+}
