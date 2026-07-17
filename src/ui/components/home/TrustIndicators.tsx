5fa2328367002d6a461e8b9abd7f783a657cb8e3
yang <juliansalvador.dev@gmail.com>
2026-07-18 03:57:52 +0800

    fix(components/home): fix divider line visibility on mobile breakpoint

--- src/ui/components/home/TrustIndicators.tsx
+++ src/ui/components/home/TrustIndicators.tsx
@@ -10,7 +10,7 @@ export default function TrustIndicators() {
         {trustIndicators.map(({ label, description, icon: Icon }, index) => (
           <article
             key={label}
-            className={`flex max-md:grow max-md:shrink max-md:basis-[min(380px,100%)] md:w-full items-center gap-4 py-4 md:py-8 px-4 lg:px-10 $  ${index === 1 && "md:relative before:left-0 [&::before,&::after]:text-background/45 [&::before,&::after]:h-[80%] [&::before,&::after]:vertical-divider-line-pseudo after:right-0"}`}>
+            className={`flex max-md:grow max-md:shrink max-md:basis-[min(380px,100%)] md:w-full items-center gap-4 py-4 md:py-8 px-4 lg:px-10 $  ${index === 1 && "md:relative before:left-0 [&::before,&::after]:hidden lg:[&::before,&::after]:block [&::before,&::after]:text-background/45 [&::before,&::after]:h-[80%] [&::before,&::after]:vertical-divider-line-pseudo after:right-0"}`}>
             <span
               aria-hidden="true"
               className="inline-block w-16 col-start-1 col-end-2 row-start-1">
