0398c7b594297157f937bc497e277ba91c8ededa
yang <juliansalvador.dev@gmail.com>
2026-07-17 22:29:59 +0800

    style(components): add content boundary

--- src/ui/components/AppHeader.tsx
+++ src/ui/components/AppHeader.tsx
@@ -8,36 +8,38 @@ import SearchIcon from "./svgs/SearchIcon";
 
 export default function AppHeader() {
   return (
-    <header className="p-4 grid grid-cols-2 lg:grid-cols-[auto_1fr_auto] items-center gap-y-4 relative after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-foreground after:origin-center">
-      <Link to={"/"} className="block w-12 sm:w-44">
-        <Logo className="sm:hidden" />
-        <TextLogo className="hidden sm:block" />
-      </Link>
-      <nav className="max-lg:col-start-1 max-lg:col-end-3">
-        <ul className="flex overflow-x-auto flex-nowrap gap-8 lg:gap-0 lg:justify-evenly scrollbar-none lg:w-fluid-width-md mx-auto">
-          {mainLinks.map((link) => (
-            <li key={link.label} className="text-nowrap">
-              <NavLink to={link.href}>{link.label}</NavLink>
-            </li>
-          ))}
-        </ul>
-      </nav>
-      <div
-        role="group"
-        aria-label="Header Actions"
-        className="max-lg:col-start-2 max-lg:col-end-3 max-lg:row-start-1 flex items-center gap-6 ml-auto">
-        <button className="flex items-center gap-2 sm:outline-1 sm:pl-2 sm:pr-3 sm:py-1 rounded-md">
-          <span className="inline-block header-actions-icon">
-            <SearchIcon />
-          </span>
-          <span className="hidden sm:inline-block">Search</span>
-        </button>
-        <button className="header-actions-icon">
-          <BagIcon />
-        </button>
-        <Link to={"/auth/login"} className="header-actions-icon">
-          <UserIcon />
+    <header className="relative after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-foreground after:origin-center">
+      <div className="w-app-content-boundary mx-auto p-4 grid grid-cols-2 lg:grid-cols-[auto_1fr_auto] items-center gap-y-4 ">
+        <Link to={"/"} className="block w-12 sm:w-44">
+          <Logo className="sm:hidden" />
+          <TextLogo className="hidden sm:block" />
         </Link>
+        <nav className="max-lg:col-start-1 max-lg:col-end-3">
+          <ul className="flex overflow-x-auto flex-nowrap gap-8 lg:gap-0 lg:justify-evenly scrollbar-none lg:w-fluid-width-md mx-auto">
+            {mainLinks.map((link) => (
+              <li key={link.label} className="text-nowrap">
+                <NavLink to={link.href}>{link.label}</NavLink>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <div
+          role="group"
+          aria-label="Header Actions"
+          className="max-lg:col-start-2 max-lg:col-end-3 max-lg:row-start-1 flex items-center gap-6 ml-auto">
+          <button className="flex items-center gap-2 sm:outline-1 sm:pl-2 sm:pr-3 sm:py-1 rounded-md">
+            <span className="inline-block header-actions-icon">
+              <SearchIcon />
+            </span>
+            <span className="hidden sm:inline-block">Search</span>
+          </button>
+          <button className="header-actions-icon">
+            <BagIcon />
+          </button>
+          <Link to={"/auth/login"} className="header-actions-icon">
+            <UserIcon />
+          </Link>
+        </div>
       </div>
     </header>
   );
